51d379ad7ad5ffc499fbb6e7cdc7fe0fe3484709
caoxuedong <catcher92@163.com>
2017-02-23 16:58:12 +0800

    nginx+tomcat时候获取真实ip地址

--- src/main/java/com/catcher92/demo/springdemo/controller/IndexController.java
+++ src/main/java/com/catcher92/demo/springdemo/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.catcher92.demo.springdemo.controller;
 
+import com.catcher92.demo.springdemo.util.WebUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ public class IndexController {
 
     @RequestMapping(value = {"","index"})
     public String index(HttpServletRequest request, Model model){
-        String ip = request.getRemoteAddr();
+        String ip = WebUtil.getRemoteAddr(request);
         // 往spring-session redis中写数据
         String message = new StringBuilder("ip:").append(ip).toString();
         request.getSession().setAttribute("message", message);
