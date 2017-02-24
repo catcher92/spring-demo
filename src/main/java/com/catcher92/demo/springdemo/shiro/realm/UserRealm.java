7ad4d0e99ef96d02c94e58a8eb8504fd69884312
caoxuedong <catcher92@163.com>
2017-02-24 11:31:51 +0800

    授权代码提取到授权回调方法中，否则如果启用了remember不经过登录认证权限会有问题

--- src/main/java/com/catcher92/demo/springdemo/shiro/realm/UserRealm.java
+++ src/main/java/com/catcher92/demo/springdemo/shiro/realm/UserRealm.java
@@ -2,12 +2,10 @@ package com.catcher92.demo.springdemo.shiro.realm;
 
 
 import com.catcher92.demo.springdemo.constant.UserConstant;
-import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
-import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.SimplePrincipalCollection;
 
@@ -24,32 +22,14 @@ public class UserRealm extends AuthorizingRealm{
         UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) token;
         String userName = usernamePasswordToken.getUsername();
         String password = String.valueOf(usernamePasswordToken.getPassword());
-        // todo 这里应该连接数据库进行查询
         if ("admin".equals(userName)) {
             if (!"123456".equals(password)) {
                 throw new IncorrectCredentialsException();
             }
-            // 角色
-            Set<String> roles = new HashSet<>(2);
-            roles.add(UserConstant.ROLES_ADMIN);
-            roles.add(UserConstant.ROLES_USER);
-            SecurityUtils.getSubject().getSession(false).setAttribute(UserConstant.ROLES, roles);
-            // 权限
-            Set<String> permissions = new HashSet<>(1);
-            permissions.add(UserConstant.PERMISSIONS_USER_ALL);
-            SecurityUtils.getSubject().getSession(false).setAttribute(UserConstant.PERMISSIONS, permissions);
             return new SimpleAuthenticationInfo(userName, password, getName());
         } else if ("test".equals(userName)){
             SimpleAuthenticationInfo info = new SimpleAuthenticationInfo();
             SimplePrincipalCollection collection = new SimplePrincipalCollection();
-            // 角色
-            Set<String> roles = new HashSet<>(1);
-            roles.add(UserConstant.ROLES_USER);
-            SecurityUtils.getSubject().getSession(false).setAttribute(UserConstant.ROLES, roles);
-            // 权限
-            Set<String> permissions = new HashSet<>(1);
-            permissions.add(UserConstant.PERMISSIONS_USER_GET);
-            SecurityUtils.getSubject().getSession(false).setAttribute(UserConstant.PERMISSIONS, permissions);
             if ("111111".equals(password)) {
                 collection.add("test1", getName());
             } else if ("123456".equals(password)) {
@@ -69,9 +49,28 @@ public class UserRealm extends AuthorizingRealm{
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
         SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
-        Session session = SecurityUtils.getSubject().getSession(false);
-        info.setRoles((Set<String>)session.getAttribute(UserConstant.ROLES));
-        info.setStringPermissions((Set<String>)session.getAttribute(UserConstant.PERMISSIONS));
+        // 如果登录认证时候有多个用户，这里会返回集合中的第一个
+        String username = (String) getAvailablePrincipal(principals);
+        Set<String> roles = null;
+        Set<String> permissions = null;
+        if ("admin".equals(username)) {
+            // 角色
+            roles = new HashSet<>(2);
+            roles.add(UserConstant.ROLES_ADMIN);
+            roles.add(UserConstant.ROLES_USER);
+            // 权限
+            permissions = new HashSet<>(1);
+            permissions.add(UserConstant.PERMISSIONS_USER_ALL);
+        } else if ("test".equals(username)) {
+            // 角色
+            roles = new HashSet<>(1);
+            roles.add(UserConstant.ROLES_USER);
+            // 权限
+            permissions = new HashSet<>(1);
+            permissions.add(UserConstant.PERMISSIONS_USER_GET);
+        }
+        info.setRoles(roles);
+        info.setStringPermissions(permissions);
         return info;
     }
 }
